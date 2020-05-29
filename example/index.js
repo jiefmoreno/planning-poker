6acb9a94e8360c8a07406ab97d32f1ae1794ed4f
jf-moreno <jiefmoreno@gmail.com>
2020-05-29 17:50:44 +0200

    refactor(*): create the new structure of the project and add typescript

---
+++ example/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const getSession = require('@planning-poker/server').init(io);
+const path = require('path')
+
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+app.get('/session/:sessionId/:userName', (req, res) => {
+  res.sendFile(__dirname + '/session.html');
+});
+app.get('/api/session/:sessionId', (req, res) => {
+  const session = getSession(req.params.sessionId);
+  res.json({
+    session
+  });
+});
+app.use('/static', express.static(path.join(__dirname, 'public')))
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
---
+++ packages/client/__tests__/client.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const client = require('..');
+
+describe('@planning-poker/client', () => {
+    it('needs tests');
+});
