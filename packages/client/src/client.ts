6786c05b7f3bc9b7889b8be78260e07b13e2549e
jf-moreno <jiefmoreno@gmail.com>
2020-06-03 18:16:53 +0200

    feat(front): add a function addTicket

--- packages/client/src/client.ts
+++ packages/client/src/client.ts
@@ -4,3 +4,7 @@ const socket = io();
 export function createSession(sessionName: string, userName: string) {
     socket.emit('new session', sessionName, userName);
 }
+
+export function addTicket(sessionId: string, value: string, userName: string) {
+    socket.emit('new ticket', sessionId, value, userName);
+}
